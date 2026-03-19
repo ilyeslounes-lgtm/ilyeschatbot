ad4efc144e6969bec6a81d3f84ca693a72146749
ilyeslounes-lgtm <ilyes.lounes@elev.ga.lbs.se>
2026-03-19 14:19:41 +0100

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -7,14 +7,24 @@ responses = {
     "bye": ["Goodbye!", "See you later!", "Bye!"]
 }
 
+questions = {
+    "what type of robot are you": ["I'm an AI programmed by a human to help you with your emotions."],
+    "can you help me": ["Yes, I can try to help you.", "Of course, tell me what's wrong."],
+    "what do you do": ["I talk with people and respond to simple questions."],
+    "are you real": ["I'm not a human, but I am a real computer program."],
+    "how old are you": ["I don't have an age like humans do."],
+    "are you similiar to Jarvis": ["i fear Jarvis is more cooler than me"]
+}
+
 fallback = [
     "I don't understand.",
     "Can you explain more?",
     "I'm not sure I understand."
 ]
+
 while True:
     user_input = input("You: ").lower()
-    
+
     found = False
 
     for key in responses:
@@ -25,8 +35,15 @@ while True:
             break
 
     if not found:
+        for key in questions:
+            if key in user_input:
+                svar = random.choice(questions[key])
+                print("Bot:", svar)
+                found = True
+                break
+
+    if not found:
         print("Bot:", random.choice(fallback))
 
     if "bye" in user_input:
         break
-
